5a461f4bb64bb996868c0924cf70426b9a8615a8
Arijit Das <babai9300@gmail.com>
2015-07-21 15:09:36 +0530

    Program to show the frequency of the character in plain text and cypher text

---
+++ day3/freq.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char ch,temp;
+   int arr[26] = {0};
+   int cypher[27] = {0};
+   int i,j,k = 7;
+   FILE *fp;
+
+   fp = fopen("s.txt","r"); // read mode
+
+   if( fp == NULL )
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   }
+   printf("The cypher text is : \n");
+   while( ( ch = fgetc(fp) ) != EOF )
+   {
+      ++arr[ch - 'a'];
+      temp = (ch -'a' + 7) % 26;
+      ++cypher[temp];
+      printf("%c",(temp + 'a'));
+   }
+   fclose(fp);
+   printf("\n\n");
+   printf("The frequency of the plain text is :\n");
+
+   ch = 'a';
+   for(i = 0 ; i < 26 ; i++)
+   {
+      printf("%c : ",ch);
+      
+      for(j = 1 ; j <= arr[i]; j++)
+      {
+         printf("-");
+      }
+      printf("\n");
+      ++ch;
+   }
+
+   printf("The frequency of the cypher text is :\n");
+   ch = 'a';
+   for(i = 1 ; i <= 26 ; i++)
+   {
+      printf("%c : ",ch);
+      
+      for(j = 1 ; j <= cypher[i]; j++)
+      {
+         printf("-");
+      }
+      printf("\n");
+      ++ch;
+   }
+
+   return 0;
+}
